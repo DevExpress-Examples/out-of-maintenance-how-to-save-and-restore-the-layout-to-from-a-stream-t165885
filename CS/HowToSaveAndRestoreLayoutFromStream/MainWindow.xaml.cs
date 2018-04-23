934c3429a93a4fdde63092c93c0eead90165ee6c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/HowToSaveAndRestoreLayoutFromStream/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace HowToSaveAndRestoreLayoutFromStream {
+    public partial class MainWindow : Window {
+        // Create a MemoryStream instance.
+        System.IO.Stream LayoutStream = new System.IO.MemoryStream();
+        public MainWindow() {
+            InitializeComponent();
+        }
+        private void buttonSave_Click(object sender, RoutedEventArgs e) {
+            // Save the layout to a stream.
+            pivotGridControl1.SaveLayoutToStream(LayoutStream);
+        }
+        private void buttonLoad_Click(object sender, RoutedEventArgs e) {
+            LayoutStream.Position = 0;
+            // Load the layout from the stream.
+            pivotGridControl1.RestoreLayoutFromStream(LayoutStream);
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to save and restore the layout to/from a stream
+
+
+<p>The following example shows how to save a DXPivotGrid Control layout to a stream using the <a href="https://documentation.devexpress.com/#WPF/DevExpressXpfPivotGridPivotGridControl_SaveLayoutToStreamtopic">PivotGridControl.SaveLayoutToStream</a> method and then, how to restore the layout from the stream using the <a href="https://documentation.devexpress.com/#WPF/DevExpressXpfPivotGridPivotGridControl_RestoreLayoutFromStreamtopic">PivotGridControl.RestoreLayoutFromStream</a>  method.</p>
+
+<br/>
+
+
