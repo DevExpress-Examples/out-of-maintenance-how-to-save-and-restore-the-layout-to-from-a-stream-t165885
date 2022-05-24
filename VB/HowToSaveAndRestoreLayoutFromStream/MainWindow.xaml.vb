6ddc06ffec533fe7866de1ccc45e3928484158d1
DevExpressExampleBot <fakeEmail>
2022-05-24 09:43:33 +0400

    Source auto update [skip ci]

--- VB/HowToSaveAndRestoreLayoutFromStream/MainWindow.xaml.vb
+++ VB/HowToSaveAndRestoreLayoutFromStream/MainWindow.xaml.vb
@@ -1,25 +1,26 @@
-﻿Imports System.Windows
+Imports System.Windows
 
 Namespace HowToSaveAndRestoreLayoutFromStream
-    Partial Public Class MainWindow
+
+    Public Partial Class MainWindow
         Inherits Window
 
         ' Create a MemoryStream instance.
         Private LayoutStream As System.IO.Stream = New System.IO.MemoryStream()
 
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
         End Sub
 
         Private Sub buttonSave_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
             ' Save the layout to a stream.
-            pivotGridControl1.SaveLayoutToStream(LayoutStream)
+            Me.pivotGridControl1.SaveLayoutToStream(LayoutStream)
         End Sub
 
         Private Sub buttonLoad_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
             LayoutStream.Position = 0
             ' Load the layout from the stream.
-            pivotGridControl1.RestoreLayoutFromStream(LayoutStream)
+            Me.pivotGridControl1.RestoreLayoutFromStream(LayoutStream)
         End Sub
     End Class
 End Namespace
