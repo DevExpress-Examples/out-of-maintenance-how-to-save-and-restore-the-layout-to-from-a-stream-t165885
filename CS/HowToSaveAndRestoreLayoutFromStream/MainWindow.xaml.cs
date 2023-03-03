1ef785061fdfa32a84cab85ac571d3232a6214d2
zakhodyaeva <margarita.zakhodyaeva@devexpress.com>
2023-03-03 16:51:01 +0400

    update example

--- CS/HowToSaveAndRestoreLayoutFromStream/MainWindow.xaml.cs
+++ CS/HowToSaveAndRestoreLayoutFromStream/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using DevExpress.Xpf.PivotGrid;
+using System.IO;
+using System.Windows;
 
 namespace HowToSaveAndRestoreLayoutFromStream {
     public partial class MainWindow : Window {
@@ -6,15 +8,31 @@ namespace HowToSaveAndRestoreLayoutFromStream {
         System.IO.Stream LayoutStream = new System.IO.MemoryStream();
         public MainWindow() {
             InitializeComponent();
+            PivotGridGroup group = pivotGridControlNew.Groups.Add(fieldYear, fieldMonth);
+            
         }
         private void buttonSave_Click(object sender, RoutedEventArgs e) {
             // Save the layout to a stream.
-            pivotGridControl1.SaveLayoutToStream(LayoutStream);
+            pivotGridControlOld.SaveLayoutToStream(LayoutStream);
         }
         private void buttonLoad_Click(object sender, RoutedEventArgs e) {
             LayoutStream.Position = 0;
+            LayoutStream.Seek(0, SeekOrigin.Begin);
             // Load the layout from the stream.
-            pivotGridControl1.RestoreLayoutFromStream(LayoutStream);
+            pivotGridControlNew.RestoreLayoutFromStream(LayoutStream);
+        }
+
+        private void pivotGridControlNew_LayoutUpgrade(object sender, PivotLayoutUpgradeEventArgs e) {
+            if (e.PreviousVersion == "1.0") {
+                var newField = new PivotGridField() {
+                    DataBinding = new DataSourceColumnBinding("Quantity"),
+                    Caption = "Quantity",
+                    Name = "fieldQuantity",
+                    Area = FieldArea.DataArea
+                };
+                pivotGridControlNew.Fields.Add(newField);
+            };
+
         }
     }
 }
--- VB/HowToSaveAndRestoreLayoutFromStream/MainWindow.xaml.vb
+++ VB/HowToSaveAndRestoreLayoutFromStream/MainWindow.xaml.vb
@@ -1,26 +1,40 @@
+Imports System.IO
 Imports System.Windows
+Imports DevExpress.Xpf.PivotGrid
 
 Namespace HowToSaveAndRestoreLayoutFromStream
 
-    Public Partial Class MainWindow
+    Partial Public Class MainWindow
         Inherits Window
 
         ' Create a MemoryStream instance.
         Private LayoutStream As System.IO.Stream = New System.IO.MemoryStream()
-
         Public Sub New()
-            Me.InitializeComponent()
-        End Sub
+            InitializeComponent()
+            Dim group As PivotGridGroup = pivotGridControlNew.Groups.Add(fieldYear, fieldMonth)
 
+        End Sub
         Private Sub buttonSave_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
             ' Save the layout to a stream.
-            Me.pivotGridControl1.SaveLayoutToStream(LayoutStream)
+            pivotGridControlOld.SaveLayoutToStream(LayoutStream)
         End Sub
-
         Private Sub buttonLoad_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
             LayoutStream.Position = 0
+            LayoutStream.Seek(0, SeekOrigin.Begin)
             ' Load the layout from the stream.
-            Me.pivotGridControl1.RestoreLayoutFromStream(LayoutStream)
+            pivotGridControlNew.RestoreLayoutFromStream(LayoutStream)
+        End Sub
+
+        Private Sub pivotGridControlNew_LayoutUpgrade(ByVal sender As Object, ByVal e As PivotLayoutUpgradeEventArgs)
+            If e.PreviousVersion = "1.0" Then
+                Dim newField = New PivotGridField() With {
+                    .DataBinding = New DataSourceColumnBinding("Quantity"),
+                    .Caption = "Quantity",
+                    .Name = "fieldQuantity",
+                    .Area = FieldArea.DataArea
+                }
+                pivotGridControlNew.Fields.Add(newField)
+            End If
         End Sub
     End Class
 End Namespace
