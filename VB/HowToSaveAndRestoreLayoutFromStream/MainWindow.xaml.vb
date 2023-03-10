babd0e7fd25fc9c448f8c57a6e0e7e438eabd816
DevExpressExampleBot <fakeEmail>
2023-03-10 16:20:54 +0400

    Source auto update [skip ci]

--- VB/HowToSaveAndRestoreLayoutFromStream/MainWindow.xaml.vb
+++ VB/HowToSaveAndRestoreLayoutFromStream/MainWindow.xaml.vb
@@ -1,35 +1,36 @@
+Imports DevExpress.Xpf.PivotGrid
 Imports System.IO
 Imports System.Windows
-Imports DevExpress.Xpf.PivotGrid
 
 Namespace HowToSaveAndRestoreLayoutFromStream
-    Partial Public Class MainWindow
+
+    Public Partial Class MainWindow
         Inherits Window
+
         ' Create a MemoryStream instance.
-        Private LayoutStream As System.IO.Stream = New System.IO.MemoryStream()
+        Private LayoutStream As Stream = New MemoryStream()
+
         Public Sub New()
-            InitializeComponent()
-            Dim group As PivotGridGroup = pivotGridControlNew.Groups.Add(fieldYear, fieldMonth)
+            Me.InitializeComponent()
+            Dim group As PivotGridGroup = Me.pivotGridControlNew.Groups.Add(Me.fieldYear, Me.fieldMonth)
         End Sub
+
         Private Sub buttonSave_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
             ' Save the layout to a stream.
-            pivotGridControlOld.SaveLayoutToStream(LayoutStream)
+            Me.pivotGridControlOld.SaveLayoutToStream(LayoutStream)
         End Sub
+
         Private Sub buttonLoad_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
             LayoutStream.Position = 0
             LayoutStream.Seek(0, SeekOrigin.Begin)
             ' Load the layout from the stream.
-            pivotGridControlNew.RestoreLayoutFromStream(LayoutStream)
+            Me.pivotGridControlNew.RestoreLayoutFromStream(LayoutStream)
         End Sub
+
         Private Sub pivotGridControlNew_LayoutUpgrade(ByVal sender As Object, ByVal e As PivotLayoutUpgradeEventArgs)
-            If e.PreviousVersion = "1.0" Then
-                Dim newField = New PivotGridField() With {
-                    .DataBinding = New DataSourceColumnBinding("Quantity"),
-                    .Caption = "Quantity",
-                    .Name = "fieldQuantity",
-                    .Area = FieldArea.DataArea
-                }
-                pivotGridControlNew.Fields.Add(newField)
+            If Equals(e.PreviousVersion, "1.0") Then
+                Dim newField = New PivotGridField() With {.DataBinding = New DataSourceColumnBinding("Quantity"), .Caption = "Quantity", .Name = "fieldQuantity", .Area = FieldArea.DataArea}
+                Me.pivotGridControlNew.Fields.Add(newField)
             End If
         End Sub
     End Class
